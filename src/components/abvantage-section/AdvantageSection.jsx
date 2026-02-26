9f5b0c4ad0fbb8ff8e0f090ba595e89e66f96f1e
Andii Karamushko <karamushko1997@gmail.com>
2026-02-26 21:28:28 +0200

    ref: add services section and imgContainer

--- src/components/abvantage-section/AdvantageSection.jsx
+++ src/components/abvantage-section/AdvantageSection.jsx
@@ -3,7 +3,7 @@ import { AdvantageListData } from "@/data/advantageListData";
 
 export const AdvantageSection = () => {
   return (
-    <section className="mx-4">
+    <section className="mx-4" id="advantage">
       <h2 className=" mb-4 text-3xl font-medium">Наші переваги</h2>
       <AdvantageList advantageList={AdvantageListData} />
     </section>
---
+++ src/data/servicesListData.js
@@ -0,0 +1,16 @@
+export const ServicesListData = [
+  {
+    title: "Генеральне приберання",
+    description: "Текст опису генерального прибирання",
+    img: "/hero.webp",
+    alt: "this is alt for general uborka",
+    id: 1,
+  },
+  {
+    title: "Вивіз сміття",
+    description: "Текст опису вивозу сміття",
+    img: "/hero.webp",
+    alt: "this is alt for viviz smittya",
+    id: 2,
+  },
+];
