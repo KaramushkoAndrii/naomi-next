678441f1b0b96911a721b1430b8412f575a9d920
Andii Karamushko <karamushko1997@gmail.com>
2026-03-07 22:43:51 +0200

    ref: changed styles for advantageList

--- src/components/abvantage-section/AdvantageList.jsx
+++ src/components/abvantage-section/AdvantageList.jsx
@@ -1,6 +1,6 @@
 export const AdvantageList = ({ advantageList }) => {
   return (
-    <ul className="flex flex-col justify-center items-center gap-4">
+    <ul className="flex flex-col justify-center items-center gap-4 md:grid md:grid-cols-2 md:gap-8 lg:grid-cols-3">
       {advantageList.map((advantageItem) => (
         <li
           key={advantageItem.id}
