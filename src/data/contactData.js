74fa1d9703e7b8c9e0de30b00c6727538950f2d6
Andii Karamushko <karamushko1997@gmail.com>
2026-03-18 22:02:36 +0200

    ref: add correct contacts

--- src/data/contactData.js
+++ src/data/contactData.js
@@ -1,26 +1,32 @@
 export const ContactData = [
   {
-    title: "0979863778",
-    src: "0979863778",
+    title: "0688282844",
+    src: "0688282844",
     type: "tel",
     id: 1,
+  },
+  {
+    title: "naomycleaning911@gmail.com",
+    src: "naomycleaning911@gmail.com",
+    type: "message",
+    id: 2,
+  },
+  {
+    title: "0688282844",
+    src: "https://t.me/+380688282844",
+    type: "link",
+    id: 3,
+  },
+  {
+    title: "0688282844",
+    src: "viber://chat?number=%2B380688282844",
+    type: "link",
+    id: 4,
   },
   {
     title: "instagram",
     src: "https://daisyui.com/components/modal/#",
     type: "link",
-    id: 2,
-  },
-  {
-    title: "naomi@gmail.com",
-    src: "naomi@gamin.com",
-    type: "message",
-    id: 3,
-  },
-  {
-    title: "0979863778",
-    src: "0979863777",
-    type: "tel",
-    id: 4,
+    id: 5,
   },
 ];
