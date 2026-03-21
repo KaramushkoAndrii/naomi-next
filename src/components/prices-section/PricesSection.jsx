aef09a843a2febabed70fd983f1afec717e4b922
Andii Karamushko <karamushko1997@gmail.com>
2026-03-21 21:38:54 +0200

    ref: changed price and add current services

--- src/components/prices-section/PricesSection.jsx
+++ src/components/prices-section/PricesSection.jsx
@@ -3,9 +3,10 @@ import { PricesData } from "@/data/pricesData";
 
 export const PricesSection = () => {
   return (
-    <section>
+    <section className="flex flex-col gap-4">
       <h2 className="text-sub-title">Наші ціни</h2>
       <PricesList list={PricesData} />
+      <span className="text-sm">* - за попередньою домовленістю</span>
     </section>
   );
 };
--- src/data/advantageListData.js
+++ src/data/advantageListData.js
@@ -1,5 +1,9 @@
 import { MdOutlineHealthAndSafety } from "react-icons/md";
 import { BsLightningCharge } from "react-icons/bs";
+import { IoDiamondOutline } from "react-icons/io5";
+import { BiLeaf } from "react-icons/bi";
+import { MdDoneOutline } from "react-icons/md";
+import { FaRegClock } from "react-icons/fa";
 
 export const AdvantageListData = [
   {
@@ -14,22 +18,22 @@ export const AdvantageListData = [
   },
   {
     id: 3,
-    text: "Безпечно",
-    icon: <MdOutlineHealthAndSafety size={60} />,
+    text: "Ідеально чисто",
+    icon: <IoDiamondOutline size={60} />,
   },
   {
     id: 4,
-    text: "Оперативність",
-    icon: <BsLightningCharge size={60} />,
+    text: "Екологічно",
+    icon: <BiLeaf size={60} />,
   },
   {
     id: 5,
-    text: "Безпечно",
-    icon: <MdOutlineHealthAndSafety size={60} />,
+    text: "Досвідчені майстри",
+    icon: <MdDoneOutline size={60} />,
   },
   {
     id: 6,
-    text: "Оперативність",
-    icon: <BsLightningCharge size={60} />,
+    text: "Гнучкий графік",
+    icon: <FaRegClock size={60} />,
   },
 ];
